ce51e92dffe79a6a8b6935347cabc616e0894bc7
arturromero <artur.romero@vix.com>
2020-03-11 10:27:19 -0300

    Fixing index dots on hover

--- machinery.js
+++ machinery.js
@@ -6,46 +6,46 @@ var canvasContext = stockCanvas.getContext('2d');
 canvasContext.imageSmoothingEnabled = true;
 
 
-var actives = [ {
-    ticker: 'AAA',
-    name: 'American Airconditioner and Airbags',
-    color: '#FFFF8C',
-    history: [15, 16, 19, 24, 28, 30, 31, 37, 39, 34, 42, 45, 50, 56, 61, 59, 57, 58, 56, 54, 66, 85, 110, 176.52]
-  },
-  {
-    ticker: 'BBB',
-    name: 'Bobby\'s Bristol Barbershop',
-    color: '#FF8FAC',
-    history: [35, 38, 42, 45, 42, 46, 51, 57, 58, 54, 61, 54, 48, 45, 42, 48, 51, 57, 62, 58, 61, 54, 48, 36.55]
-  },
-  {
-    ticker: 'CCC',
-    name: 'Charles & Cody Carpentry',
-    color: '#88FFFF',
-    history: [76, 72, 78, 86, 82, 78, 71, 76, 68, 66, 68, 67, 70, 76, 82, 78, 71, 67, 68, 68, 51, 43, 39, 30.43]
-  }
-];
-
-// var actives = [
-//   {
+// var actives = [ {
 //     ticker: 'AAA',
 //     name: 'American Airconditioner and Airbags',
 //     color: '#FFFF8C',
-//     history: [0.005, 0.003, 0.008, 0.007, 0.009, 0.012, 0.014, 0.008, 0.0091]
+//     history: [15, 16, 19, 24, 28, 30, 31, 37, 39, 34, 42, 45, 50, 56, 61, 59, 57, 58, 56, 54, 66, 85, 110, 176.52]
 //   },
 //   {
 //     ticker: 'BBB',
 //     name: 'Bobby\'s Bristol Barbershop',
 //     color: '#FF8FAC',
-//     history: [0.028, 0.030, 0.035, 0.037, 0.034, 0.032, 0.031, 0.026, 0.02437]
+//     history: [35, 38, 42, 45, 42, 46, 51, 57, 58, 54, 61, 54, 48, 45, 42, 48, 51, 57, 62, 58, 61, 54, 48, 36.55]
 //   },
 //   {
 //     ticker: 'CCC',
 //     name: 'Charles & Cody Carpentry',
 //     color: '#88FFFF',
-//     history: [0.02, 0.016, 0.015, 0.012, 0.016, 0.023, 0.0286, 0.0426, 0.04894]
+//     history: [76, 72, 78, 86, 82, 78, 71, 76, 68, 66, 68, 67, 70, 76, 82, 78, 71, 67, 68, 68, 51, 43, 39, 30.43]
 //   }
 // ];
+
+var actives = [
+  {
+    ticker: 'AAA',
+    name: 'American Airconditioner and Airbags',
+    color: '#FFFF8C',
+    history: [0.005, 0.003, 0.008, 0.007, 0.009, 0.012, 0.014, 0.008, 0.0091]
+  },
+  {
+    ticker: 'BBB',
+    name: 'Bobby\'s Bristol Barbershop',
+    color: '#FF8FAC',
+    history: [0.028, 0.030, 0.035, 0.037, 0.034, 0.032, 0.031, 0.026, 0.02437]
+  },
+  {
+    ticker: 'CCC',
+    name: 'Charles & Cody Carpentry',
+    color: '#88FFFF',
+    history: [0.02, 0.016, 0.015, 0.012, 0.016, 0.023, 0.0286, 0.0426, 0.04894]
+  }
+];
 
 var canvasHeight = stockCanvas.offsetHeight;
 var canvasWidth = stockCanvas.offsetWidth;
@@ -102,9 +102,10 @@ function drawCanvas (canvas, source, highlight) {
   console.log('Drawing Canvas');
   canvas.strokeStyle = color + (highlight ? (highlight == source.ticker ? '': '55') : '99');
   canvas.lineWidth = 2;
+  canvas.setLineDash([]);
 
   var stepHorizontal = canvasWidth / (source.history.length -1);
-  var figure = calcScale(240, extremities.min, extremities.max);
+  var figure = calcScale(canvasHeight, extremities.min, extremities.max);
   var stride = figure.stride * figure.scale;
 
   canvas.beginPath();
@@ -194,7 +195,7 @@ function renderDot (parent, xx, yy, color, value) {
 }
 function drawGradient (canvas, source, steph, iy, scale, stride, color) {
   var gradient = canvas.createLinearGradient(0, 0, 0, canvasHeight);
-  gradient.addColorStop(0, `${source.color}99`);
+  gradient.addColorStop(0, `${source.color}77`);
   gradient.addColorStop(1, `${source.color}01`);
   canvas.fillStyle = gradient;
 
